4012579aa7339023b6b3a81156a55c59900be66a
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-22 12:59:19 -0400

    Feature: Displays temp and city name.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { config } from "./config.js";
 
-let mainDiv = document.querySelector("div");
+let p = document.querySelector("p");
 
 fetch(config())
 
 .then(response => response.json())
 .then(data => {
-    console.log(data);
-    mainDiv.textContent = JSON.stringify(data);
+    document.querySelector("#name").innerHTML = data["name"];
+    document.querySelector("#temperature").innerHTML = data["main"]["temp"];
+    document.querySelector("#description").innerHTML = data["weather"][0]["main"];
+    console.log(data["name"])
+    p.textContent = JSON.stringify(data);
 })
 .catch(err => alert("Wrong city name!"))
