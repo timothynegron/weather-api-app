d9f423462bca4a929751bf57b19cb69af1451e3b
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-22 14:47:32 -0400

    Version 1 complete.

--- main.js
+++ main.js
@@ -17,12 +17,33 @@ function search(){
     fetch(config(document.querySelector("input").value))
     .then(response => response.json())
     .then(data => {
-        document.querySelector("#name").innerHTML = data["name"];
-        document.querySelector("#temperature").innerHTML = data["main"]["temp"];
-        document.querySelector("#description").innerHTML = data["weather"][0]["main"];
-        console.log(data["name"])
-        p.textContent = JSON.stringify(data);
+        setLocationName(data["name"])
+        setWeatherDescription(data["weather"][0]["description"]);
+        setTemperature(data["main"]["temp"]);
+        setWeatherIcon(data["weather"][0]["icon"]);
     })
     .catch(err => alert("Wrong city name!"))
+    document.querySelector("input").value = "";
+}
+
+function setWeatherDescription(description){
+    document.querySelector("#description").innerHTML = description;
+    
+}
+
+function setWeatherIcon(code){
+    document.querySelector("img").src = "http://openweathermap.org/img/wn/" + code + "@4x.png";
+}
+
+function setLocationName(name){
+    document.querySelector("#name").innerHTML = name;
+}
+
+function setTemperature(temp){
+    document.querySelector("#temperature").innerHTML = kelvinToFahrenheit(temp);
+}
+
+function kelvinToFahrenheit(kelvin){
+    return Math.ceil(((kelvin - 273.15) * 9/5 + 32)) + "&#176" + "F";
 }
 
