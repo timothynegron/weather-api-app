87063ad729cd9f40bfcfc3b6611d9452f8c00f39
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-22 13:24:39 -0400

    Feature: Adds functional search bar.

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ import { config } from "./config.js";
 
 let p = document.querySelector("p");
 
-fetch(config())
 
-.then(response => response.json())
-.then(data => {
-    document.querySelector("#name").innerHTML = data["name"];
-    document.querySelector("#temperature").innerHTML = data["main"]["temp"];
-    document.querySelector("#description").innerHTML = data["weather"][0]["main"];
-    console.log(data["name"])
-    p.textContent = JSON.stringify(data);
-})
-.catch(err => alert("Wrong city name!"))
+main();
+
+function main(){
+    setEventListener();
+}
+
+function setEventListener() {
+    document.querySelector("button").addEventListener("click", search)
+}
+
+function search(){
+    fetch(config(document.querySelector("input").value))
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#name").innerHTML = data["name"];
+        document.querySelector("#temperature").innerHTML = data["main"]["temp"];
+        document.querySelector("#description").innerHTML = data["weather"][0]["main"];
+        console.log(data["name"])
+        p.textContent = JSON.stringify(data);
+    })
+    .catch(err => alert("Wrong city name!"))
+}
+
