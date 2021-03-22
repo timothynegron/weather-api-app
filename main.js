38f187f787c36edecbe275c3fbb0d3c5cb0d0ba6
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-22 12:42:54 -0400

    Feature: Gets JSON strong from API.

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import { secretSauce } from "./config.js";
-console.log(secretSauce());
+import { config } from "./config.js";
+
+let mainDiv = document.querySelector("div");
+
+fetch(config())
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    mainDiv.textContent = JSON.stringify(data);
+})
+.catch(err => alert("Wrong city name!"))
